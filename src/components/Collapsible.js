34f5467829647a27640bbd6cbad937d92933f049
Juan Posadas <jposadasc@gmail.com>
2022-04-28 11:39:47 +0800

    new task working

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,67 +1,66 @@
-import React, {useState} from "react"
+import React, {useState} from "react";
 
-import useCollapse from 'react-collapsed';
+import useCollapse from "react-collapsed";
 
-export default function Collapsible (props) {
+export default function Collapsible(props) {
+  const {
+    key,
+    taskId,
+    name,
+    description,
+    isExpanded,
+    updateTask,
+    setCurrentTaskId,
+  } = props;
 
-    const { key, name, description, isExpanded, updateTask, setCurrentTaskId} = props
+  console.log(props)
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    console.log(formData.get("name"), formData.get("description"));
+    updateTask(taskId, formData.get("name"), formData.get("description"));
+    // updateTask(event)
+  }
 
+  function handleEdit(event) {
+    // setCurrentTaskId(key)
+    console.log(event);
+    // updateTask(event)
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        console.log(event)
-        updateTask(event)
-    }
+  return isExpanded ? (
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="What are you working on?"
+          className="form-input"
+          name="name"
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          className="form-input"
+          name="description"
+        />
+        <div>
+          <button name="isExpanded" type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  ) : (
+    <div className="task--container">
+      <div className="task-info">
+        <div>{name}</div>
+        <div>{description}</div>
+      </div>
 
-    function handleEdit(event) {
-        setCurrentTaskId(key)
-        console.log(event)
-        updateTask(event)
-       
-
-    }
-
-
-    return (
-      
-           isExpanded ? 
-                <div className = "form-container">
-                    < form className = "form" >
-                        <input
-                            type="text"
-                            placeholder="What are you working on?"
-                            className="form-input"
-                            name="name"
-                            value={name}
-                            onChange={(event) => updateTask(event)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Description"
-                            className="form-input"
-                            name="description"
-                            value={description}
-                            onChange={(event) => updateTask(event) }
-                        />
-                        <div>
-                            <button name="isExpanded" onClick = {handleSubmit}>Save</button>
-                        </div>
-                    </form >
-                </div >
-            :
-                <div className="task--container">
-                    <div className="task-info">
-                        <div>{name}</div>
-                        <div>{description}</div>
-                    </div>
-                    
-                    <button name="isExpanded" onClick={handleEdit}>Edit</button>
-                    
-                </div>
-           
-        
-    )
+      <button name="isExpanded" onClick={handleEdit}>
+        Edit
+      </button>
+    </div>
+  );
 }
-  
-
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,77 +1,65 @@
-import React, {useEffect, useState} from "react"
-import { nanoid } from 'nanoid'
-import useCollapse from 'react-collapsed';
-import Collapsible from "./Collapsible"
-
+import React, {useEffect, useState} from "react";
+import {nanoid} from "nanoid";
+import useCollapse from "react-collapsed";
+import Collapsible from "./Collapsible";
 
 export default function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  let lastElement = tasks.length - 1;
 
-    const [tasks, setTasks] = useState([])
-    let lastElement = tasks.length - 1
-    const [currentTaskId, setCurrentTaskId] = useState(
-        (tasks[lastElement] && tasks[lastElement].id) || ""
-    )
+  function createNewTask() {
+    const newTask = {
+      id: nanoid(),
+      name: "",
+      description: "",
+      isExpanded: true,
+    };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  }
 
-    function createNewTask() {
-        const newTask = {
-            id: nanoid(),
-            name: "",
-            description: "",
-            isExpanded: true  
-        }
-        setTasks(prevTasks => [...prevTasks, newTask])
-        setCurrentTaskId(newTask.id)
-    }
+  const taskList = tasks.map((task) => (
+    <Collapsible
+      key={task.id}
+      taskId={task.id}
+      name={task.name}
+      description={task.description}
+      updateTask={updateTask}
+      isExpanded={task.isExpanded}
+    />
+  ));
 
+  function updateTask(taskId, name, description) {
+    let newTasks = tasks.reduce((res, elem, idx) => {
+      if (elem.id === taskId) {
+        res.push({
+          ...elem,
+          name: name,
+          description: description,
+          isExpanded: !elem.isExpanded,
+        });
+      } else {
+        res.push(elem);
+      }
 
+      return res;
+    }, []);
 
+    setTasks(newTasks);
+  }
 
-    
-    const taskList = tasks.map(task => (
-        
-            <Collapsible
-                key ={task.id}
-                name = {task.name}
-                description = {task.description}
-                updateTask = {updateTask}
-                isExpanded={task.isExpanded}
-                setCurrentTaskId = {setCurrentTaskId}
+  function deleteTask(event, taskId) {
+    event.stopPropagation();
+    setTasks((oldTasks) => {
+      return oldTasks.filter((oldTask) => oldTask.id != taskId);
+    });
+  }
 
-            /> 
-        )
-    )
-
-    function updateTask(event) {
-        const{name, value, type} = event.target
-       setTasks(prevTasks => {
-           const newArray = []
-           for (let i = 0; i < prevTasks.length; i++) {
-               const oldTask = prevTasks[i]
-               if (oldTask.id === currentTaskId) {
-                   newArray.push({ ...oldTask, [name]: name ==="isExpanded" ? !oldTask.isExpanded : value})
-               } else {
-                   newArray.push(oldTask)
-               }
-           }
-           return newArray
-       })
-
-    }
-
-    function deleteTask(event, taskId) {
-        event.stopPropagation()
-        setTasks(oldTasks => {
-            return oldTasks.filter(oldTask => oldTask.id !=taskId)
-        })
-        
-    }
-
-
-    
-    return (
-        <div className ="taskList--container">
-            {taskList}
-            <button className="taskList--add" onClick={createNewTask}>Add Task </button>
-        </div>
-    )
+  return (
+    <div className="taskList--container">
+      {taskList}
+      <button className="taskList--add" onClick={createNewTask}>
+        Add Task{" "}
+      </button>
+    </div>
+  );
 }
